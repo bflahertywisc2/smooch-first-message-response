4a6fa21972594e37ee98fb3552341f983ab46249
spasiu <stasi@smooch.io>
2017-10-12 22:33:06 -0400

    Check X-API-Key header in webhook requests git push origin master

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const response = process.env.RESPONSE;
 const appId = process.env.SMOOCH_APP_ID;
 const keyId = process.env.SMOOCH_ACCOUNT_KEY_ID;
 const secret = process.env.SMOOCH_ACCOUNT_SECRET;
+const webhookSecret = process.env.WEBHOOK_SECRET;
 
 const smooch = new Smooch({
 	scope: 'account',
@@ -22,27 +23,23 @@ express()
 	.listen(port, () => console.log(`Running on port ${port}`));
 
 async function webhookHandler(req, res) {
-	try {
-		// if not an appUser message event, bail
-		if (req.body.trigger !== 'message:appUser') return res.end();
-		
-		const userId = req.body.appUser._id;
-		const messageData = await smooch.appUsers.getMessages(appId, userId);
+	// if secret is set and doesn't match, bail
+	if (webhookSecret && headers['x-api-key'] !== webhookSecret) return res.end();
 
-		// if these are not the first messages, bail
-		if (messageData.messages > req.body.messages) return res.end();
+	// if not an appUser message event, bail
+	if (req.body.trigger !== 'message:appUser') return res.end();
+	
+	const userId = req.body.appUser._id;
+	const messageData = await smooch.appUsers.getMessages(appId, userId);
 
-		await smooch.appUsers.sendMessage(appId, userId, {
-			text: response,
-			type: 'text',
-			role: 'appMaker'
-		});
+	// if these are not the first messages, bail
+	if (messageData.messages > req.body.messages) return res.end();
 
-		res.end();
-	}
+	await smooch.appUsers.sendMessage(appId, userId, {
+		text: response,
+		type: 'text',
+		role: 'appMaker'
+	});
 
-	catch (err) {
-		console.error(err);
-		res.end();
-	}
+	res.end();
 }
